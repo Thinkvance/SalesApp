55043311a16df86e8a47a5e8f56ad28a111fd2c9
Thinkvance <thinkvance@gmail.com>
2025-05-19 17:50:24 +0530

    cache feature removed!

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,5 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-// main.js
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then((reg) => {
-        // First install: controller is null → ignore
-        if (!navigator.serviceWorker.controller) return;
-
-        // When a new SW is found...
-        reg.addEventListener("updatefound", () => {
-          const newWorker = reg.installing;
-          newWorker.addEventListener("statechange", () => {
-            // Once it's installed and there's an active controller,
-            if (
-              newWorker.state === "installed" &&
-              navigator.serviceWorker.controller
-            ) {
-              window.dispatchEvent(new Event("swUpdated"));
-            }
-          });
-        });
-      })
-      .catch(console.error);
-  });
-}
 createRoot(document.getElementById("root")).render(<App />);
