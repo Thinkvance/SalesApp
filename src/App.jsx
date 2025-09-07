3f524fbbc19d9530da963263534e486297344b7c
Thinkvance <thinkvance@gmail.com>
2025-09-07 19:47:12 +0530

    salesv4 updated! ready to test!

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ import ReviewManagement from "./ReviewManagement";
 function App() {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
-
+  console.log("Test 07-09-2025!");
   useEffect(async () => {
     const getPermission = async () => {
       try {
