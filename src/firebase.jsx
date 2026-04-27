076f06e33c26aadc7e03372f1c843ce409f32c8b
Thinkvance solutions <thinkvance@Thinkvances-MacBook-Air.local>
2026-04-27 18:00:30 +0530

    main updated

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAwHe1Ci22MD09r-skn7OZLyYBdEX35L74",
-  authDomain: "shiphitmobileapppickup-4d0a1.firebaseapp.com",
-  projectId: "shiphitmobileapppickup-4d0a1",
-  storageBucket: "shiphitmobileapppickup-4d0a1.appspot.com",
-  messagingSenderId: "977746945332",
-  appId: "1:977746945332:web:17c4aa3b217b35cf58f161",
+  apiKey: "AIzaSyCJ-uGdjFO1kRT9DrRvbnpbEXQ5QXoKpeo",
+  authDomain: "shiphit-salesapp-test-env.firebaseapp.com",
+  projectId: "shiphit-salesapp-test-env",
+  storageBucket: "shiphit-salesapp-test-env.firebasestorage.app",
+  messagingSenderId: "866319555778",
+  appId: "1:866319555778:web:60cb04c99e04580170af5b",
 };
 
 // Initialize Firebase
